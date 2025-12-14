d0b12199f6105e09705da11a18655b3850da0477
Raghav-Bhatia4047 <u24ai054@aid.svnit.ac.in>
2025-12-15 03:28:41 +0530

    login

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
+   path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile_view, name='profile'),
 ]
